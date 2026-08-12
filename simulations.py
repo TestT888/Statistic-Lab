11f48956c489dd65637d92eb234f80c6bd2e19fd
TestT888 <gojkoreljic11@gmail.com>
2026-08-12 16:25:17 +0200

    dodana Metropolis-Hastings funkcija

--- simulations.py
+++ simulations.py
@@ -1,10 +1,11 @@
-# Monte Carlo procena broja pi
+
 
 import math
 import random
-from getopt import error
+from distributions import normal
 
 
+# Monte Carlo procena broja pi
 def monte_carlo_pi(n):
     inside = 0
     for _ in range(n):
@@ -43,4 +44,33 @@ def clt_demo(sampler, sample_size, num_trials):
         sample = [sampler() for _ in range (sample_size)]
         means.append(sum(sample) / sample_size)
     return means
+
+# Metropolis-Hastings
+# Pravilo prihvatanja
+# prihvati x' ako je: log(slucajan_broj_0_do_1)<log_density(x')-log_density(x)
+
+def metropolis_hastings(log_density, initial, n_samples, proposal_std=1.0, burn_in=500):
+    samples = []
+    current = initial
+    current_log_p = log_density(current)
+
+    total_steps = n_samples + burn_in   #burn-in period
+
+    for i in range(total_steps):            #provera da li je prosao burn-in period
+        proposal = normal(current, proposal_std)
+        proposal_log_p = log_density(proposal)
+
+        log_accept_ratio = proposal_log_p - current_log_p  # pozitivno predlog je gusci, negativno predlog je redji
+        # ako je predlog bolji ili jednako dobar  uvek se prihvata
+        # ako je predlog losiji prihvati ga samo sa verovatnocom -> exp(log_acept_ratio)
+        # sto je predlog losiji exp(ratio) je blize nuli
+        if log_accept_ratio >= 0 or random.random() < math.exp(log_accept_ratio):
+            current = proposal
+            current_log_p = proposal_log_p
+        if i>= burn_in:
+            samples.append(current)        # tek posle burn-in perioda pozicije se cuvaju
+
+
+    return samples
+
 
